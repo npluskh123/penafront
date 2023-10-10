0506739a2cc2d8071d7a0743551032e9e4adfb8a
Никита Храмцов <nikitahramcov@MacBook-Pro.local>
2023-10-10 17:08:23 +0900

    11111

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axio from 'axios';
 const instance = axio.create({
   //baseURL: 'http://localhost:4444',
   //baseURL: process.env.PORT || 8080,
-  baseURL: 'http://93.183.75.58:3000',
+  baseURL: 'http://93.183.75.58:8080',
 });
 
 //вшивать авторизацию
